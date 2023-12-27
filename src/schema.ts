c4e1bd92c2e86f4dcb73b0187ee12fc01495ccb5
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-12-28 12:53:34 +1300

    Add error handling and validation

--- src/schema.ts
+++ src/schema.ts
@@ -1,6 +1,9 @@
 import { makeExecutableSchema } from '@graphql-tools/schema'
 import { GraphQLContext } from './context'
 import { Link, Comment } from '@prisma/client'
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library'
+import { GraphQLError } from 'graphql'
+import { assert } from 'console'
 
 const typeDefinitions = /* GraphQL */ `
   type Query {
@@ -73,10 +76,13 @@ const resolvers = {
       args: { description: string; url: string },
       context: GraphQLContext
     ) {
+      const postUrl = isValidUrl(args.url)
+      if (!postUrl)
+        return new GraphQLError(`Cannot post link with invalid url ${args.url}`)
       const newLink = await context.prisma.link.create({
         data: {
           description: args.description,
-          url: args.url,
+          url: postUrl.href,
         },
       })
       return newLink
@@ -86,15 +92,51 @@ const resolvers = {
       args: { linkId: string; body: string },
       context: GraphQLContext
     ) {
-      const newComment = await context.prisma.comment.create({
-        data: {
-          linkId: parseInt(args.linkId),
-          body: args.body,
-        },
-      })
+      const linkId = parseIntSafe(args.linkId)
+      if (!args.body) throw new GraphQLError('Comment must not be blank')
+      if (!linkId)
+        throw new GraphQLError(
+          `Cannot post comment on link with invalid id '${args.linkId}'.`
+        )
+      const newComment = await context.prisma.comment
+        .create({
+          data: {
+            linkId: linkId,
+            body: args.body,
+          },
+        })
+        .catch((err: unknown) => {
+          console.log('yo', err)
+          if (
+            err instanceof PrismaClientKnownRequestError &&
+            err.code === 'P2003'
+          ) {
+            return Promise.reject(
+              new GraphQLError(
+                `Cannot post comment on non-existing link with id '${args.linkId}'.`
+              )
+            )
+          }
+          return Promise.reject(err)
+        })
       return newComment
     },
   },
+}
+
+function parseIntSafe(value: string): number | null {
+  if (/^(\d+)$/.test(value)) {
+    return parseInt(value, 10)
+  }
+  return null
+}
+
+function isValidUrl(urlString: string) {
+  try {
+    return new URL(urlString)
+  } catch (e) {
+    return null
+  }
 }
 
 export const schema = makeExecutableSchema({
