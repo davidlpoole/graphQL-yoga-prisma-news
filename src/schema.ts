4f197707f6a60637ecaf44cd6b48b50f9c3ee1a8
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-12-28 09:59:52 +1300

    Add comments model

--- src/schema.ts
+++ src/schema.ts
@@ -6,16 +6,24 @@ const typeDefinitions = /* GraphQL */ `
   type Query {
     info: String!
     feed: [Link!]!
+    comment(id: ID!): Comment
   }
 
   type Mutation {
     postLink(url: String!, description: String!): Link!
+    postCommentOnLink(linkId: ID!, body: String!): Comment!
   }
 
   type Link {
     id: ID!
     description: String!
     url: String!
+    comments: [Comment!]!
+  }
+
+  type Comment {
+    id: ID!
+    body: String!
   }
 `
 const resolvers = {
@@ -23,11 +31,21 @@ const resolvers = {
     info: () => `This is the API of Hackernews Clone`,
     feed: (parent: unknown, args: {}, context: GraphQLContext) =>
       context.prisma.link.findMany(),
+    comment: (parent: unknown, args: { id: string }, context: GraphQLContext) =>
+      context.prisma.comment.findUnique({
+        where: { id: parseInt(args.id) },
+      }),
   },
   Link: {
     id: (parent: Link) => parent.id,
     description: (parent: Link) => parent.description,
     url: (parent: Link) => parent.url,
+    comments: (parent: Link, args: {}, context: GraphQLContext) =>
+      context.prisma.comment.findMany({
+        where: {
+          linkId: parent.id,
+        },
+      }),
   },
   Mutation: {
     async postLink(
@@ -39,6 +57,18 @@ const resolvers = {
         data: {
           description: args.description,
           url: args.url,
+        },
+      })
+    },
+    async postCommentOnLink(
+      parent: unknown,
+      args: { linkId: string; body: string },
+      context: GraphQLContext
+    ) {
+      return await context.prisma.comment.create({
+        data: {
+          linkId: parseInt(args.linkId),
+          body: args.body,
         },
       })
     },
